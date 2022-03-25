cc8229dd5b4168affe0488d421fe2a007f6d8d47
qbentil <Bentilshadrack72@gmail.com>
2022-03-25 09:50:45 +0000

    [NEW] Add new_user compoent

---
+++ components/new_user.php
@@ -0,0 +1,61 @@
+<section class="content">
+    
+<div class="col-md-6">
+<!-- general form elements -->
+<div class="box box-primary">
+  <!-- <div class="box-header with-border">
+    <h3 class="box-title">Quick Example</h3>
+  </div> -->
+  <!-- /.box-header -->
+  <!-- form start -->
+  <form role="form">
+    <div class="box-body">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-user"></i></span>
+            <input type="text" id="name" name="name" class="form-control" placeholder="eg.: John Doe ">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+            <input type="email" id="email" name="email" class="form-control" placeholder="eg.: doe@example.com">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="phone">Phone</label>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+            <input type="text" id="phone" name="phone" class="form-control" placeholder="eg.: +1234567890">
+        </div>
+      </div>
+
+      <div class="form-group" style="display: flex; justify-content: space-evenly; align-items:center ">
+            <label>
+                <input type="radio" name="user-type" class="flat-red" checked>
+                User
+            </label>
+            <label>
+                <input type="radio" name="user-type" class="flat-red">
+                Writer
+            </label>
+            <label>
+                <input type="radio" name="user-type" class="flat-red" >
+                Admin
+            </label>
+        </div>
+
+    </div>
+    <!-- /.box-body -->
+
+    <div class="box-footer">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</div>
+<!-- /.box -->
+
+</div>
+</section>
