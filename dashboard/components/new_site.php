6794c4b8c16535d1f46143144d20e686c96c85b5
qbentil <Bentilshadrack72@gmail.com>
2022-03-28 13:44:37 +0000

    [Update]:Design new Site form with photo preview

--- dashboard/components/new_site.php
+++ dashboard/components/new_site.php
@@ -1,3 +1,32 @@
+<style>
+.image-preview-input {
+    position: relative;
+	overflow: hidden;
+	margin: 0px;    
+    color: #333;
+    background-color: #fff;
+    border-color: #ccc;    
+}
+.image-preview-input input[type=file] {
+	position: absolute;
+	top: 0;
+	right: 0;
+	margin: 0;
+	padding: 0;
+	font-size: 20px;
+	cursor: pointer;
+	opacity: 0;
+	filter: alpha(opacity=0);
+}
+.image-preview-input-title {
+    margin-left:2px;
+}
+
+textarea{
+  resize: vertical;
+}
+</style>
+
 <section class="content">
 <div class="col-md-6">
 <!-- general form elements -->
@@ -5,50 +34,51 @@
   <!-- form start -->
   <form role="form" method="POST" autocomplete="off">
     <div class="box-head">
-    <?php new_user(); ?>
+    <?php new_site(); ?>
     </div>
     <div class="box-body">
       <div class="form-group">
         <label for="name">Name</label>
         <div class="input-group">
             <span class="input-group-addon"><i class="fa fa-user"></i></span>
-            <input type="text" id="name" name="name" class="form-control" placeholder="eg.: John Doe " required>
+            <input type="text" id="name" name="name" class="form-control" placeholder="eg.: Boti Water falls " required>
         </div>
       </div>
       <div class="form-group">
-        <label for="email">Email</label>
+        <label for="location">Location</label>
         <div class="input-group">
             <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-            <input type="email" id="email" name="email" class="form-control" placeholder="eg.: doe@example.com" required>
+            <input type="location" id="location" name="location" class="form-control" placeholder="eg.: Volta Region" required>
         </div>
       </div>
       <div class="form-group">
-        <label for="phone">Phone</label>
+        <label for="about">About</label>
         <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-            <input type="text" id="phone" name="phone" class="form-control" placeholder="eg.: +1234567890" required>
+            <span class="input-group-addon"><i class="fa fa-file-text"></i></span>
+            <textarea  type="text" id="about" name="about" class="form-control" placeholder="Brief description of site" required></textarea>
         </div>
       </div>
-      <div class="form-group" style="display: flex; justify-content: space-evenly; align-items:center ">
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="1" checked required>
-                User
-            </label>
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="3" required>
-                Writer
-            </label>
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="2" required>
-                Admin
-            </label>
-        </div>
+      <div class="input-group image-preview">
+          <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- didn't give a name cos doesn't send on POST/GET -->
+          <span class="input-group-btn">
+              <!-- image-preview-clear button -->
+              <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+                  <span class="glyphicon glyphicon-remove"></span> Clear
+              </button>
+              <!-- image-preview-input -->
+              <div class="btn btn-default image-preview-input">
+                  <span class="glyphicon glyphicon-folder-open"></span>
+                  <span class="image-preview-input-title">Browse</span>
+                  <input type="file" accept="image/png, image/jpeg, image/gif" name="site_img" required/> <!-- This sends image on POST/GET -->
+              </div>
+          </span>
+      </div><!-- /input-group image-preview [TO HERE]--> 
 
     </div>
     <!-- /.box-body -->
 
     <div class="box-footer">
-      <button type="submit" name="btn_add_user" class="btn btn-primary">Submit</button>
+      <button type="submit" name="btn_add_site" class="btn btn-primary">Submit</button>
     </div>
   </form>
 </div>
@@ -56,3 +86,4 @@
 
 </div>
 </section>
+
