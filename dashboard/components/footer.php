7aa3df02e1db0168a555ececd75cdc5e0a97d90d
qbentil <Bentilshadrack72@gmail.com>
2022-03-28 21:34:51 +0000

    Add Scripts to general app scope

--- dashboard/components/footer.php
+++ dashboard/components/footer.php
@@ -54,6 +54,22 @@
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 
+<!-- CK Editor -->
+<script src="bower_components/ckeditor/ckeditor.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+
+
+<!-- text editor -->
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1')
+    //bootstrap WYSIHTML5 - text editor
+    $('.textarea').wysihtml5()
+  })
+</script>
 <!-- init dtb -->
 <script>
 
