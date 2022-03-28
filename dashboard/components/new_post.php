ad4d84983a41f064b5f7150050a4a5224cb2eab0
qbentil <Bentilshadrack72@gmail.com>
2022-03-28 21:35:54 +0000

    Complete new popsts

--- dashboard/components/new_post.php
+++ dashboard/components/new_post.php
@@ -1,58 +1,81 @@
-<section class="content">
-<div class="col-md-6">
-<!-- general form elements -->
-<div class="box box-primary">
-  <!-- form start -->
-  <form role="form" method="POST" autocomplete="off">
-    <div class="box-head">
-    <?php new_user(); ?>
-    </div>
-    <div class="box-body">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-user"></i></span>
-            <input type="text" id="name" name="name" class="form-control" placeholder="eg.: John Doe " required>
+<style>
+.image-preview-input {
+    position: relative;
+	overflow: hidden;
+	margin: 0px;    
+    color: #333;
+    background-color: #fff;
+    border-color: #ccc;    
+}
+.image-preview-input input[type=file] {
+	position: absolute;
+	top: 0;
+	right: 0;
+	margin: 0;
+	padding: 0;
+	font-size: 20px;
+	cursor: pointer;
+	opacity: 0;
+	filter: alpha(opacity=0);
+}
+.image-preview-input-title {
+    margin-left:2px;
+}
+
+</style>
+
+<section class="row content">
+    <div class="col-md-12">
+        <!-- general form elements -->
+        <div class="box box-primary">
+            <!-- form start -->
+            <form role="form" method="POST" autocomplete="off" enctype="multipart/form-data">
+                <div class="box-head">
+                <?php new_post(); ?>
+                </div>
+                <div class="box-body">
+                <div class="form-group col-md-8">
+                    <label for="title">Title</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                        <input type="text" id="title" name="title" class="form-control" placeholder="Post title " required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <!-- <label for="about">About</label> -->
+                    <div class="input-group  col-md-12">
+                        <textarea id="editor1" name="content" class="form-control">Post content</textarea>
+                        <!-- <textarea  type="text" id="about" name="about" class="form-control" placeholder="Brief description of site" required></textarea> -->
+                    </div>
+                </div>
+                    <!-- /.box -->
+                <div class="input-group col-md-8 image-preview">
+                    <label for="">Post banner</label>
+                    <input type="text" class="form-control image-preview-filename" disabled="disabled"> <!-- didn't give a name cos doesn't send on POST/GET -->
+                    <span class="input-group-btn">
+                        <!-- image-preview-clear button -->
+                        <button type="button" class="btn btn-default image-preview-clear" style="display:none;">
+                            <span class="glyphicon glyphicon-remove"></span> Clear
+                        </button>
+                        <!-- image-preview-input -->
+                        <div class="btn btn-default image-preview-input">
+                            <span class="glyphicon glyphicon-folder-open"></span>
+                            <span class="image-preview-input-title">Browse</span>
+                            <input type="file" accept="image/png, image/jpeg, image/gif" name="post_img" required/> <!-- This sends image on POST/GET -->
+                        </div>
+                    </span>
+                </div><!-- /input-group image-preview [TO HERE]--> 
+
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                <button type="submit" name="btn_add_post" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
-      </div>
-      <div class="form-group">
-        <label for="email">Email</label>
-        <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-            <input type="email" id="email" name="email" class="form-control" placeholder="eg.: doe@example.com" required>
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="phone">Phone</label>
-        <div class="input-group">
-            <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-            <input type="text" id="phone" name="phone" class="form-control" placeholder="eg.: +1234567890" required>
-        </div>
-      </div>
-      <div class="form-group" style="display: flex; justify-content: space-evenly; align-items:center ">
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="1" checked required>
-                User
-            </label>
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="3" required>
-                Writer
-            </label>
-            <label>
-                <input type="radio" name="user_type" class="flat-red" value="2" required>
-                Admin
-            </label>
-        </div>
+        <!-- /.box -->
 
     </div>
-    <!-- /.box-body -->
-
-    <div class="box-footer">
-      <button type="submit" name="btn_add_user" class="btn btn-primary">Submit</button>
-    </div>
-  </form>
-</div>
-<!-- /.box -->
-
-</div>
 </section>
