c54db2be4b2781ad5102e9273ca74a64be5aec6a
qbentil <Bentilshadrack72@gmail.com>
2022-03-29 22:25:43 +0000

    Add Change PAssword feat

---
+++ dashboard/change-password.php
@@ -0,0 +1,26 @@
+<?php 
+  require_once "./components/functions.php";
+  require_once "./components/head.php" ;
+  session_start();
+
+?>
+
+<?php require_once "./components/navbar.php" ?>
+  <!-- Left side column. contains the logo and sidebar -->
+<?php require_once "./components/sidebar.php" ?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+
+<!-- Tabs -->
+<?php 
+
+
+
+?>
+</div>
+
+<?php require_once "./components/footer.php" ?>
+
+
